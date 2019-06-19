e93c668358c3c8ba0fe0f2cd8045793395803d8b
Kevin Sims <kevinsims@Kevins-MacBook-Pro.local>
2019-06-19 12:57:22 -0700

    actions being created

--- src/reducers/starWarsReducer.js
+++ src/reducers/starWarsReducer.js
@@ -1,13 +1,37 @@
-import /* we need our action types here*/ "../actions";
+import {
+  FETCH_CHARACTERS_START,
+  FETCH_CHARACTERS_SUCCESS,
+  FETCH_CHARACTERS_ERROR
+} from "../actions";
 const initialState = {
-  characters: []
   // Array characters, Boolean fetching, null error.
+  characters: [],
+  error: '',
+  isLoading: false,
 };
 export const charsReducer = (state = initialState, action) => {
   switch (action.type) {
     // Fill me in with the important reducers
     // action types should be FETCHING, SUCCESS and FAILURE
     // your switch statement should handle all of these cases.
+    case FETCH_CHARACTERS_START:
+      return {
+        ...state,
+        error: '',
+        isLoading: true
+      }
+    case FETCH_CHARACTERS_SUCCESS:
+      return {
+        ...state,
+        isLoading:false,
+        characters: action.payload
+      }
+    case FETCH_CHARACTERS_ERROR:
+      return {
+        ...state,
+        isLoading: false,
+        error: 'DID NOT LOAD CHECK THE CODE'
+      }
     default:
       return state;
   }
